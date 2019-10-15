8821d16a761b64870e148e123eb42aa65d97c8dd
Martin Förster <martinfoerster95@gmx.de>
2019-10-15 18:46:59 +0200

    Example added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 ## [1.0.2] - 15.10.2019.
 
 * Updated dependencies
+* Provide example
 
 ## [1.0.1] - 13.10.2019.
 
---
+++ example/main.dart
@@ -0,0 +1,41 @@
+
+import 'package:bloc_event/bloc_event.dart';
+
+class CounterState {
+  final int counter;
+
+  CounterState({this.counter});
+
+  CounterState copyWith({
+    int counter,
+  }) {
+    return CounterState(
+      counter: counter ?? this.counter,
+    );
+  }
+}
+
+abstract class CounterEvent<E> extends Event<CounterBloc, CounterState, E> {}
+
+class CounterBloc extends EventBloc<CounterEvent, CounterState> {
+
+  @override
+  CounterState get initialState => CounterState(counter: 0);
+
+}
+
+class CounterIncrement extends CounterEvent<CounterIncrement> {
+  final int value;
+
+  CounterIncrement(this.value);
+
+  @override
+  Stream<CounterState> onTriggered(bloc, state, event) async* {
+    yield state.copyWith(counter: state.counter+event.value);
+  }
+}
+
+void main() {
+  final counterBloc = CounterBloc();
+  counterBloc.add(CounterIncrement(2));
+}
